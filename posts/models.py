03db0febf0bb2d1ade0a971e64631a8385d5eeef
mahmoudtraman <memo24112002@gmail.com>
2023-10-25 21:14:30 +0200

    design post model pt1

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+'''
+    - title
+    - author
+    - content
+    - image
+    - publish_data
+    - tags
+'''
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=50000)
+    publish_date = models.DateTimeField()
+    
