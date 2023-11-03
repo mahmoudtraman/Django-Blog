9319fa8f40e623659576cf326260381eb414312b
mahmoudtraman <memo24112002@gmail.com>
2023-11-03 05:45:00 -1000

    add list , detail using cbv

---
+++ posts/views2.py
@@ -0,0 +1,11 @@
+from django.views import generic
+from .models import post
+
+
+class PostList(generic.ListView):
+    model = post
+
+
+
+class PostDetail(generic.DetailView):
+        model =post
